418dc8347b14a25126d07864ea5342ce0da94926
Creeper Lv <creeperlv@users.noreply.github.com>
2025-05-26 04:38:29 +1000

    Working on the scanner.

--- LibCLCC.C/Headers/gp_scanner.h
+++ LibCLCC.C/Headers/gp_scanner.h
@@ -5,8 +5,17 @@
 #define GPCHAR char
 #endif
 #include <stdio.h>
+#include <stdint.h>
 #include <stdbool.h>
+#include <string.h>
 
+#ifndef GPSCANNER_STRLEN
+#define GPSCANNER_STRLEN strlen
+#ifdef GPSCANNER_STRLEN
+#undef GPSCANNER_STRLEN_RT
+#endif
+#define GPSCANNER_STRLEN_RT size_t
+#endif
 typedef struct gpscanner_enclosure_pair
 {
     GPCHAR *L;
@@ -21,32 +30,47 @@ typedef struct gpscanner_config
     GPCHAR **line_comment_identifier;
     gpscanner_enclosure_pair *closed_comments_identifiers;
     gpscanner_enclosure_pair *quote_identifiers;
-    /// @brief when streamed_process is enabled, multiple GPCHAR* allocation will happen during the
-    /// process.
-    bool streamed_process;
 } gpscanner_config;
 
 /// @brief Should pass gpscanner_task*
 typedef GPCHAR (*gpscanner_read)(void *);
 /// @brief Should pass gpscanner_task*
+typedef GPCHAR *(*gpscanner_head)(void *);
+/// @brief Should pass gpscanner_task*
 typedef size_t (*gpscanner_length)(void *);
+typedef struct gpscanner_pos
+{
+    char *id;
+    int64_t LineNumber;
+    int64_t Column;
+} gpscanner_pos;
+
 typedef struct gpscanner_segment
 {
     GPCHAR *Head;
     int Length;
     bool is_predefined;
+    bool is_predefined_single;
+    GPCHAR predefined_char;
     gpscanner_enclosure_pair *quote_identifier;
-    struct gpscanner_segment *L;
-    struct gpscanner_segment *R;
+    struct gpscanner_segment *Pre;
+    struct gpscanner_segment *Next;
 } gpscanner_segment;
 typedef struct gpscanner_task
 {
     gpscanner_config *config;
+    /// @brief when streamed_process is enabled, multiple GPCHAR* allocation will happen during the
+    /// process.
+    bool streamed_process;
     gpscanner_read read_func;
+    gpscanner_head head_func;
     gpscanner_length length_func;
     void *payload;
+    char *id;
 } gpscanner_task;
 
+gpscanner_segment *LibCLCC_gp_scanner_scan_file(FILE *f);
+gpscanner_segment *LibCLCC_gp_scanner_scan_filename(char *fn);
 gpscanner_segment *LibCLCC_gp_scanner_scan(gpscanner_task *task);
 gpscanner_segment *LibCLCC_gp_scanner_scan_step(gpscanner_task *task);
 void LibCLCC_gpscanner_destory_segment(gpscanner_segment *head);
--- LibCLCC.C/Source/gp_scanner.c
+++ LibCLCC.C/Source/gp_scanner.c
@@ -1,15 +1,93 @@
 #include "../Headers/gp_scanner.h"
 #include <stdlib.h>
 
+inline static bool Match(GPCHAR *list, GPCHAR Testee)
+{
+    GPSCANNER_STRLEN_RT len = GPSCANNER_STRLEN(list);
+    for (GPSCANNER_STRLEN_RT i = 0; i < len; i++)
+    {
+        if (list[i] == Testee)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+inline static gpscanner_segment *create_segment()
+{
+    gpscanner_segment *segment = malloc(sizeof(gpscanner_segment));
+    segment->is_predefined = false;
+    segment->is_predefined_single = false;
+    segment->Head = NULL;
+    segment->Pre = NULL;
+    segment->Next = NULL;
+    segment->quote_identifier = NULL;
+    return segment;
+}
 gpscanner_segment *LibCLCC_gp_scanner_scan(gpscanner_task *task)
 {
+    gpscanner_segment *head = create_segment();
+    gpscanner_segment *current = head;
+    GPCHAR *Head;
+    if (task->streamed_process == true)
+    {
+    }
+    else
+    {
+        Head = task->head_func(task);
+    }
+    int InnerPos = 0;
+    int SegPos = 0;
+    bool IsInEnclosure = false;
+    while (1)
+    {
+        GPCHAR c = task->read_func(task);
+        if (IsInEnclosure)
+        {
+        }
+        else
+        {
+            if (Match(task->config->predefined_single_char_segment, c))
+            {
+                current->is_predefined = true;
+                current->is_predefined_single = true;
+                current->predefined_char = c;
+                gpscanner_segment *next = create_segment();
+                current->Next = next;
+                current = next;
+                goto SKIP_END;
+            }
+        }
+
+    SKIP:
+
+        SegPos++;
+
+    SKIP_END:
+
+        InnerPos++;
+    }
+
     return NULL;
 }
 gpscanner_segment *LibCLCC_gp_scanner_scan_step(gpscanner_task *task);
 void LibCLCC_gpscanner_destory_segment(gpscanner_segment *head)
 {
-    head->L = NULL;
-    head->R = NULL;
+    head->Pre = NULL;
+    head->Next = NULL;
     free(head);
 }
-void LibCLCC_gpscanner_destory_segment_linked_list(gpscanner_segment *head);
+void LibCLCC_gpscanner_destory_segment_linked_list(gpscanner_segment *head)
+{
+    gpscanner_segment *ptr = head;
+    while (1)
+    {
+        if (ptr == NULL)
+        {
+            return;
+        }
+        gpscanner_segment *nPtr = ptr->Next;
+        LibCLCC_gpscanner_destory_segment(ptr);
+        ptr = nPtr;
+    }
+}
